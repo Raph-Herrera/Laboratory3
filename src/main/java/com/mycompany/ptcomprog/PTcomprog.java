6157322fe60913ca4196d87139ad0418c26143ce
Raph-Herrera <silanherrera10@gmail.com>
2024-09-23 15:03:42 +0800

    laboratory 3

---
+++ src/main/java/com/mycompany/ptcomprog/PTcomprog.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ptcomprog;
+
+/**
+ *
+ * @author Bon Jovi
+ */
+public class PTcomprog {
+
+    public static void main(String[] args) {
+        String myName = ("Raphael Silan Herrera");
+        String myBirthday = ("July 22, 2006");
+        int myAge = 17;
+        double myAverage = 93.11;
+        char myFavoriteletter = 'R';
+        String myFavoritecolor = ("Lavander Blue");
+        String myMotto = ("If it looks stupid and it works, then it's not stupid");
+        
+        System.out.println("Full name:" +myName);
+        System.out.println("Birthday:" +myBirthday);
+        System.out.println("Age:" +myAge);
+        System.out.println("Average:" +myAverage);
+        System.out.println("Favorite Letter:" +myFavoriteletter);
+        System.out.println("Favorite Color:" +myFavoritecolor);
+        System.out.println("Motto:" +myMotto);
+        
+        
+        
+    }
+}
